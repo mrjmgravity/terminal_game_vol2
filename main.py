bd15ff140f72ac068cc4a82f4e33f067892577af
mrjmgravity <118164654+mrjmgravity@users.noreply.github.com>
2024-04-09 20:48:31 +0200

    is name
    i added set nickname to character

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+INTRO_TEXT = "Welcome in game called Arcadius\nDo you want play the game?\n0 - Exit\n1 - Play"
+
 should_continue = True
 while should_continue:
-    print("Welcome in game called Arcadius\nDo you want play the game?")
-    print("0 - Exit\n1- Play")
+    print(INTRO_TEXT)
 
     intro_choice = int(input("What is your choice? "))
     if intro_choice not in [0, 1]:
@@ -12,5 +13,17 @@ while should_continue:
         print("Goodbye")
         should_continue = False
     elif intro_choice == 1:
-        print("Welcome to the world of Arcadius")
+        print("Perfect, introduce yourself")
+        break
+
+is_name = True
+while is_name:
+    choose_name = input("What is your nickname? ")
+    print(f"Is this your nickname {choose_name}? Or you want to change it\n0 - Change the nickname\n"
+          f"1 - Confirm nickname ")
+    name_choice = int(input("Your choice: "))
+    if name_choice == 0:
+        continue
+    elif name_choice == 1:
+        print(f"Hello {choose_name}, welcome in Arcadius.")
         break
