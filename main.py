e830576af31f90f348a90ad694ef920092e66339
mrjmgravity <118164654+mrjmgravity@users.noreply.github.com>
2024-04-09 21:01:10 +0200

    Abilitty and refactor
    Added abillity dictionary and refactor some ugly things. Added else for invalid choice

--- main.py
+++ main.py
@@ -1,5 +1,32 @@
 INTRO_TEXT = "Welcome in game called Arcadius\nDo you want play the game?\n0 - Exit\n1 - Play"
 
+abilities = {
+    "Attack damage": {
+        "points": 1,
+        "description": "Attack damage is important with base attack and its connected with Dexterity and Luck"
+    },
+    "Defense": {
+        "points": 1,
+        "description": "Celkový obrana sa ráta z bodov obrany + obratnosti."
+    },
+    "Dexterity": {
+        "points": 1,
+        "description": "Dexterity is important with defense and attack."
+    },
+    "Skill": {
+        "points": 1,
+        "description": "SKill is important with base attack and with critical strike."
+    },
+    "Health": {
+        "points": 50,
+        "description": "Health can be restored after battle."
+    },
+    "Luck": {
+        "points": 1,
+        "description": "Luck is important for critical strike."
+    }
+}
+
 should_continue = True
 while should_continue:
     print(INTRO_TEXT)
@@ -19,7 +46,7 @@ while should_continue:
 is_name = True
 while is_name:
     choose_name = input("What is your nickname? ")
-    print(f"Is this your nickname {choose_name}? Or you want to change it\n0 - Change the nickname\n"
+    print(f"Is this your nickname {choose_name}? Do you want to change it?\n0 - Change the nickname\n"
           f"1 - Confirm nickname ")
     name_choice = int(input("Your choice: "))
     if name_choice == 0:
@@ -27,3 +54,6 @@ while is_name:
     elif name_choice == 1:
         print(f"Hello {choose_name}, welcome in Arcadius.")
         break
+    else:
+        print("Invalid choice")
+        continue
