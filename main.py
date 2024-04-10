a2eda4d352f8f2f9b40d9b72a499e98a524a11a6
mrjmgravity <118164654+mrjmgravity@users.noreply.github.com>
2024-04-10 18:37:58 +0200

    Trycatch
    Added try catch to secure the code from crashing when user type wrong input

--- main.py
+++ main.py
@@ -30,30 +30,37 @@ abilities = {
 should_continue = True
 while should_continue:
     print(INTRO_TEXT)
+    try:
+        intro_choice = int(input("What is your choice? "))
+        if intro_choice not in [0, 1]:
+            print("You have to choose between 0 and 1")
+            continue
 
-    intro_choice = int(input("What is your choice? "))
-    if intro_choice not in [0, 1]:
-        print("You have to choose between 0 and 1")
-        continue
-
-    if intro_choice == 0:
-        print("Goodbye")
-        should_continue = False
-    elif intro_choice == 1:
-        print("Perfect, introduce yourself")
-        break
+        if intro_choice == 0:
+            print("Goodbye")
+            should_continue = False
+        elif intro_choice == 1:
+            print("Perfect, introduce yourself")
+            break
+    except ValueError:
+        print("Please enter number")
 
 is_name = True
 while is_name:
     choose_name = input("What is your nickname? ")
     print(f"Is this your nickname {choose_name}? Do you want to change it?\n0 - Change the nickname\n"
           f"1 - Confirm nickname ")
-    name_choice = int(input("Your choice: "))
-    if name_choice == 0:
-        continue
-    elif name_choice == 1:
-        print(f"Hello {choose_name}, welcome in Arcadius.")
-        break
-    else:
-        print("Invalid choice")
-        continue
+    try:
+        name_choice = int(input("Your choice: "))
+        if name_choice == 0:
+            continue
+        elif name_choice == 1:
+            print(f"Hello {choose_name}, welcome in Arcadius.")
+            break
+        else:
+            print("Invalid choice")
+            continue
+    except ValueError:
+        print("Please enter number")
+
+        
