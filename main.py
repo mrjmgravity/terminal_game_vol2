b0bd2dbf1f9ad96b78bdb60116e6f94d4b483190
mrjmgravity <118164654+mrjmgravity@users.noreply.github.com>
2024-04-10 20:15:53 +0200

    assign abilities
    added two functions to print abilities and abilities points and added code block to assign 7 points to abilities

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ abilities = {
     },
     "Defense": {
         "points": 1,
-        "description": "Celkový obrana sa ráta z bodov obrany + obratnosti."
+        "description": "Defense is calculating defense + dexterity."
     },
     "Dexterity": {
         "points": 1,
@@ -26,6 +26,21 @@ abilities = {
         "description": "Luck is important for critical strike."
     }
 }
+
+
+def print_abilities_options():
+    for i, ability in enumerate(abilities.keys()):
+        ability_option = str(i + 1) + ' - ' + ability
+        if ability == "Život":
+            ability_option += " " + "- jeden bod pridá 5 života"
+        print(ability_option)
+
+
+def print_abilities_points():
+    for k, v in abilities.items():
+        print(k + " - " + str(v["points"]), end=", ")
+    print("\n")
+
 
 should_continue = True
 while should_continue:
@@ -56,11 +71,36 @@ while is_name:
             continue
         elif name_choice == 1:
             print(f"Hello {choose_name}, welcome in Arcadius.")
-            break
+            is_name = False
         else:
             print("Invalid choice")
             continue
     except ValueError:
         print("Please enter number")
 
-        
+    print("This is your abilities: ")
+    print_abilities_points()
+
+    abilities_picked = False
+    ability_picked_count = 0
+    while not abilities_picked:
+        print_abilities_options()
+        ability_choice = int(input("You have " + str(7 - ability_picked_count) + " points to assign: "))
+
+        if ability_choice.is_integer() and int(ability_choice) in list(range(1, len(abilities) + 1)):
+            chosen_ability_name = list(abilities.keys())[int(ability_choice) - 1]
+            chosen_ability = abilities[chosen_ability_name]
+        else:
+            print("invalid input")
+            continue
+
+        if chosen_ability_name == "Health":
+            chosen_ability["points"] += 5
+        else:
+            chosen_ability["points"] += 1
+
+        print_abilities_points()
+
+        ability_picked_count += 1
+        if ability_picked_count == 7:
+            abilities_picked = True
